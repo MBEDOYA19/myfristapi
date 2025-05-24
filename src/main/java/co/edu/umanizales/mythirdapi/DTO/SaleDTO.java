8cd137c37d3f894728d227dcbe22d3b3a6a8ad0f
MBEDOYA19 <mbedoya112465@umanizales.edu.co>
2025-05-24 12:43:08 -0500

    Adelanto del trabajo final parte 2

--- src/main/java/co/edu/umanizales/mythirdapi/DTO/SaleDTO.java
+++ src/main/java/co/edu/umanizales/mythirdapi/DTO/SaleDTO.java
@@ -1,14 +1,17 @@
 package co.edu.umanizales.mythirdapi.DTO;
 
 import co.edu.umanizales.mythirdapi.model.ProductSale;
-
+import lombok.Data;
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
 public class SaleDTO {
-    private String seller;
-    private String store;
-    private List<ProductSale> products;
+    private String sellerIdentification;
+    private String storeCode;
+    private List<ProductSale> productCode;
     private LocalDate saleDate;
-
+    private String code;
+    private int quantity;
+    private double subtotal;
 }
--- src/main/java/co/edu/umanizales/mythirdapi/model/ProductSale.java
+++ src/main/java/co/edu/umanizales/mythirdapi/model/ProductSale.java
@@ -1,7 +1,15 @@
 package co.edu.umanizales.mythirdapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ProductSale {
     private String code;
     private int quantity;
     private double subtotal;
+    public Seller getSeller;
 }
